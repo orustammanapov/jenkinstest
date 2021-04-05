20dc5ac02ad9bcc137077796d29d79c24f6c1584
orustammanapov <orustammanapov@gmail.com>
2021-04-05 23:23:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+3.4
+===
+
+A Symfony project created on January 6, 2021, 1:52 pm.
---
+++ src/AppBundle/Controller/MyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyController extends Controller
+{
+    /**
+     * @Route("/best", name="home")
+     */
+    public function indexAction(Request $request)
+    {
+        dump(new \stdClass());
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
